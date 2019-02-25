819a072d7d7e29aab7a857fd20b5936007077d67
linkel <ironcarbide@gmail.com>
2019-02-25 13:51:47 -0800

    Rearranged organization of files and added login POST

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
-const knex = require('knex');
-const knexConfig = require('./knexfile.js');
-const db = knex(knexConfig.development);
+const db = require('./dataaccess/users');
 
 const server = express();
 
@@ -16,3 +14,41 @@ server.get('/', (req, res) => {
     res.send("Server is up!");
 });
 
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+    if (user.username && user.password) {
+        const hash = bcrypt.hashSync(user.password,4);
+        user.password = hash;
+        db.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(err => {
+            res.status(500).json(error);
+        })
+    }
+})
+
+server.post('/api/login', (req, res) => {
+    let user = req.body;
+    if (user.username && user.password) {
+        db.findBy({username: user.username})
+        .first()
+        .then(info => {
+            if (info && bcrypt.compareSync(user.password, info.password)) {
+                res.status(200).json({message: `Welcome ${info.username}`})
+            } else {
+                res.status(401).json({message: "Invalid credentials"})
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err);
+        })
+    } else {
+        res.status(400).json({message: "Please give a username and password"})
+    }
+})
+
+const port = 5000;
+server.listen(port, () => console.log(`server running on port ${port}`))
