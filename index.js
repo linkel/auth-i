706dc6b876a76909cc186e827f93a8aac99199f6
linkel <ironcarbide@gmail.com>
2019-02-25 14:08:17 -0800

    Added GET request behind a restriction middleware, no sessions yet

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const db = require('./dataaccess/users');
-
+const session = require('express-session')
 const server = express();
 
 server.use(helmet());
@@ -50,5 +50,34 @@ server.post('/api/login', (req, res) => {
     }
 })
 
+function restricted(req, res, next) {
+    let { username, password } = req.headers;
+  
+    if (username && password) {
+      db.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+          console.log(error)
+        res.status(500).json({message: "No credentials provided."});
+      });
+    } else {
+      res.status(400).json({message:"No credentials provided."});
+    }
+  }
+
+server.get('/api/users', restricted, (req, res) => {
+    db.find()
+    .then(users => {
+        res.json(users);
+    })
+    .catch(err => res.send(err));
+})
 const port = 5000;
 server.listen(port, () => console.log(`server running on port ${port}`))
